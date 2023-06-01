00ca11cf2db818952500214ca60c6e2ce98e298a
Erhan Ertem <eertem@gmail.com>
2023-06-01 16:07:33 +0300

    Add mobile menu button functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+
 const input = document.getElementById('link-input');
 const linkForm = document.getElementById('link-form');
 const errMsg = document.getElementById('err-msg');
 
+btn.addEventListener('click', navToggle);
 linkForm.addEventListener('submit', formSubmit);
 
+//Toggle Mobile Menu
+function navToggle() {
+	btn.classList.toggle('open');
+	menu.classList.toggle('flex');
+	menu.classList.toggle('hidden');
+}
+
+//Form Submission Hnadler
 function formSubmit(event) {
 	event.preventDefault();
 	console.log(input.value);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./**/*.{html,js}'],
+	content: ['./*.html', './js/script.js'],
 	theme: {
 		extend: {
 			colors: {
