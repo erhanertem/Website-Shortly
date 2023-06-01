4e10d7f6e8cea3948b2ad337514c80bd764c13af
Erhan Ertem <eertem@gmail.com>
2023-06-01 12:50:50 +0300

    JS link validation script for the submission form

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const input = document.getElementById('link-input');
+const linkForm = document.getElementById('link-form');
+const errMsg = document.getElementById('err-msg');
+
+linkForm.addEventListener('submit', formSubmit);
+
+function formSubmit(event) {
+	event.preventDefault();
+	console.log(input.value);
+	if (input.value === '') {
+		errMsg.innerHTML = 'Please enter something';
+		input.classList.add('border-red');
+	} else if (!validURL(input.value)) {
+		errMsg.innerHTML = 'Please enter a valid URL';
+		input.classList.add('border-red');
+	} else {
+		errMsg.innerHTML = '';
+		input.classList.remove('border-red');
+		console.log('Success');
+	}
+}
+
+//Validate a URL
+function validURL(str) {
+	var pattern = new RegExp(
+		'^(https?:\\/\\/)?' + // protocol
+			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+			'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+			'(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+			'(\\#[-a-z\\d_]*)?$',
+		'i',
+	); // fragment locator
+	return !!pattern.test(str);
+}
